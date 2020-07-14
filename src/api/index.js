4dc341d7fa25ddbe35e9fa8202cb6c02ee6b76c2
erik.duisheev <erik.duisheev@zensoft.io>
2020-07-15 05:15:29 +0600

    Update server url from localhost to heroku url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://flight-optimizer-server.herokuapp.com/api",
 });
 api.defaults.headers.post["Content-Type"] = "application/json";
 export default api;
